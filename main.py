b66d441bdeeb188ca2fc366725db52e35e5bd3bc
muditvir <mvirmani02@gmail.com>
2022-07-17 21:58:58 +0530

    latest

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+ans=input("are u trying?")
+
+
+# In[18]:
+
+
+if ans=="yes":
+    print("keep going")
+else:
+    print("this isn't good")
+
+
+# In[ ]:
+
+
+
+
