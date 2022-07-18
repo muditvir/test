796ca4b313b26cc3aad6f6a41fac7dbb4f30bc33
muditvir <mvirmani02@gmail.com>
2022-07-18 17:22:49 +0530

    line extended

--- main.py
+++ main.py
@@ -4,14 +4,14 @@
 # In[17]:
 
 
-ans=input("are u trying?")
+ans=input("are u trying? ")
 
 
 # In[18]:
 
 
 if ans=="Y":
-    print("keep going")
+    print("keep going...you will definitely rock one day")
 else:
     print("this isn't good")
 
